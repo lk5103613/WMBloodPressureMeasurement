9dd7342e2817ace7c31ec8ccf7f64364b7dc0d4f
joselyncui <15895026586@163.com>
2015-01-29 10:41:38 +0800

    change fragment_device

--- src/com/wm/fragments/DeviceFragment.java
+++ src/com/wm/fragments/DeviceFragment.java
@@ -1,19 +1,79 @@
 package com.wm.fragments;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
+import com.wm.adapter.DeviceListAdapter;
+import com.wm.entity.DeviceDataSet;
+import com.wm.entity.DeviceInfo;
+import com.wm.entity.OptionEnum;
+import com.wm.entity.TypeEnum;
 import com.wm.wmbloodpressuremeasurement.R;
 
 public class DeviceFragment extends Fragment {
+	@InjectView(R.id.recyclerView)
+	RecyclerView mRecyclerView;
+	
+	@InjectView(R.id.btn_change)
+	Button mBtnUpdate;
+	DeviceDataSet deviceDataSet;
+	
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_device, container, false);
+		View view = inflater
+				.inflate(R.layout.fragment_device, container, false);
+		ButterKnife.inject(this, view);
+
+		return view;
 	}
-	
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		initData();
+		// 创建一个线性布局管理器
+		LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+		mRecyclerView.setLayoutManager(layoutManager);
+		
+		final DeviceListAdapter adapter = new DeviceListAdapter(deviceDataSet);
+		mRecyclerView.setAdapter(adapter);
+		mRecyclerView.setItemAnimator(new DefaultItemAnimator());  
+		
+		mBtnUpdate.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				deviceDataSet.option= OptionEnum.ITEM_UPDATE;
+				adapter.notifyDataSetChanged();
+				
+			}
+		});
+	}
+
+	private void initData() {
+		deviceDataSet = new DeviceDataSet();
+		deviceDataSet.option = OptionEnum.ITEM_DELETE;
+		ArrayList<DeviceInfo> deviceInfos = new ArrayList<>();
+
+		DeviceInfo deviceInfo = null;
+		for (int i = 0; i < 30; i++) {
+			deviceInfo = new DeviceInfo(TypeEnum.TURGOSCOPE, "血压计 -" + i);
+			deviceInfos.add(deviceInfo);
+		}
+		deviceDataSet.deviceInfos = deviceInfos;
+	}
+
 }
