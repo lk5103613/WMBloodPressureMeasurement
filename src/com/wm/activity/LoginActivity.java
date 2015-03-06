1f3bc36fb889669816e6bde99303f046c70027fb
Like <524148211@qq.com>
2015-03-06 13:03:28 +0800

    Optimize login code.

--- src/com/wm/activity/LoginActivity.java
+++ src/com/wm/activity/LoginActivity.java
@@ -6,23 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
-import android.view.View.OnFocusChangeListener;
-import android.widget.EditText;
 import android.widget.ScrollView;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
+import butterknife.OnFocusChange;
 
-public class LoginActivity extends Activity implements OnFocusChangeListener {
+public class LoginActivity extends Activity {
 	
 	@InjectView(R.id.login_scroll)
 	ScrollView mScrollView;
 	@InjectView(R.id.scroll_inner)
 	View mInner;
-	@InjectView(R.id.txt_username)
-	EditText mTxtUsername;
-	@InjectView(R.id.txt_pwd)
-	EditText mTxtPwd;
 
 	private Context mContext;
 
@@ -34,10 +29,8 @@ public class LoginActivity extends Activity implements OnFocusChangeListener {
 
 		mContext = LoginActivity.this;
 		
-		mTxtUsername.setOnFocusChangeListener(this);
-		mTxtPwd.setOnFocusChangeListener(this);
 	}
-
+	
 	@OnClick(R.id.btn_login)
 	public void login(View v) {
 		Intent intent = new Intent(mContext, MainActivity.class);
@@ -48,6 +41,13 @@ public class LoginActivity extends Activity implements OnFocusChangeListener {
 	@OnClick({R.id.txt_username, R.id.txt_pwd})
 	public void clickUsername(View v) {
 		if(v.isFocused()) {
+			scrollToBottom();
+		}
+	}
+	
+	@OnFocusChange({R.id.txt_pwd, R.id.txt_username})
+	public void onFocusChange(View v, boolean hasFocus) {
+		if(hasFocus) {
 			scrollToBottom();
 		}
 	}
@@ -67,13 +67,6 @@ public class LoginActivity extends Activity implements OnFocusChangeListener {
 				mScrollView.scrollTo(0, offset);
 			}
 		}, 100);
-	}
-
-	@Override
-	public void onFocusChange(View v, boolean hasFocus) {
-		if(hasFocus) {
-			scrollToBottom();
-		}
 	}
 
 }
