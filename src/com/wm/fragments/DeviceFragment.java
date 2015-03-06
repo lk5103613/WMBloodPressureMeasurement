a7dc53ac7f609b669f0d49a085f37d2cb54c879d
joselyncui <15895026586@163.com>
2015-03-06 15:31:10 +0800

    change dialog position

--- src/com/wm/fragments/DeviceFragment.java
+++ src/com/wm/fragments/DeviceFragment.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
