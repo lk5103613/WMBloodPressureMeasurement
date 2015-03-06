32c2b56e628618e9c23db47b40c0b81ceb73e1c0
Like <524148211@qq.com>
2015-03-06 13:04:44 +0800

    Optimize the code of login.

--- src/com/wm/activity/LoginActivity.java
+++ src/com/wm/activity/LoginActivity.java
@@ -13,7 +13,7 @@ import butterknife.OnClick;
 import butterknife.OnFocusChange;
 
 public class LoginActivity extends Activity {
-	
+
 	@InjectView(R.id.login_scroll)
 	ScrollView mScrollView;
 	@InjectView(R.id.scroll_inner)
@@ -28,30 +28,30 @@ public class LoginActivity extends Activity {
 		ButterKnife.inject(this);
 
 		mContext = LoginActivity.this;
-		
+
 	}
-	
+
 	@OnClick(R.id.btn_login)
 	public void login(View v) {
 		Intent intent = new Intent(mContext, MainActivity.class);
 		startActivity(intent);
 		finish();
 	}
-	
-	@OnClick({R.id.txt_username, R.id.txt_pwd})
+
+	@OnClick({ R.id.txt_username, R.id.txt_pwd })
 	public void clickUsername(View v) {
-		if(v.isFocused()) {
+		if (v.isFocused()) {
 			scrollToBottom();
 		}
 	}
-	
-	@OnFocusChange({R.id.txt_pwd, R.id.txt_username})
+
+	@OnFocusChange({ R.id.txt_pwd, R.id.txt_username })
 	public void onFocusChange(View v, boolean hasFocus) {
-		if(hasFocus) {
+		if (hasFocus) {
 			scrollToBottom();
 		}
 	}
-	
+
 	private void scrollToBottom() {
 		Handler mHandler = new Handler();
 
@@ -60,7 +60,8 @@ public class LoginActivity extends Activity {
 				if (mScrollView == null || mInner == null) {
 					return;
 				}
-				int offset = mInner.getMeasuredHeight() - mScrollView.getHeight();
+				int offset = mInner.getMeasuredHeight()
+						- mScrollView.getHeight();
 				if (offset < 0) {
 					offset = 0;
 				}
