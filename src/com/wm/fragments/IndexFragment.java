4a46398f92dcff228719c0847374fddc69682b53
joselyncui <15895026586@163.com>
2015-03-16 09:33:29 +0800

    change login position

--- src/com/wm/fragments/IndexFragment.java
+++ src/com/wm/fragments/IndexFragment.java
@@ -1,11 +1,16 @@
 package com.wm.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.wm.activity.LoginActivity;
 import com.wm.activity.R;
 
 public class IndexFragment extends Fragment {
@@ -13,7 +18,27 @@ public class IndexFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+		setHasOptionsMenu(true);// ÏÔÊ¾fragmentµÄmenu
 		return inflater.inflate(R.layout.fragment_index, container, false);
 	}
+	
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		getActivity().getMenuInflater().inflate(R.menu.main, menu);
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		switch (item.getItemId()) {
+		case R.id.action_login:
+			Intent intent = new Intent(getActivity(), LoginActivity.class);
+			startActivity(intent);
+			break;
+		default:
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
 	
 }
