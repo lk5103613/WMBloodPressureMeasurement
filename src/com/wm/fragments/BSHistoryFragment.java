fb113f1d6805de12caacf80abeb0069a23a4a45b
joselyncui <15895026586@163.com>
2015-03-30 21:01:01 +0800

    add bs history line chart

--- src/com/wm/fragments/BSHistoryFragment.java
+++ src/com/wm/fragments/BSHistoryFragment.java
@@ -1,5 +1,10 @@
 package com.wm.fragments;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.os.Bundle;
 import android.os.Handler;
@@ -10,10 +15,13 @@ import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.wm.activity.R;
 import com.wm.db.HistoryDBManager;
 import com.wm.entity.BSResult;
+import com.wm.utils.DateUtil;
 import com.wm.utils.UUIDS;
 
 public class BSHistoryFragment extends BaseHistoryFragment {
@@ -25,6 +33,7 @@ public class BSHistoryFragment extends BaseHistoryFragment {
 	private Handler mHandler;
 	private BluetoothGattCharacteristic mCommandCharac;
 	private HistoryDBManager historyDBManager;
+	private List<BSResult> bsResults;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,10 +45,19 @@ public class BSHistoryFragment extends BaseHistoryFragment {
 		mHandler = new Handler();
 
 		initLineChart();
-		addEmptyData();
+		
 		historyDBManager = HistoryDBManager.getInstance(getActivity());
 		
 		return view;
+	}
+	@Override
+	public void onResume() {
+		super.onResume();
+		bsResults = historyDBManager.getAllBsResults();
+		System.out.println("result " + bsResults.size());
+		addEmptyData();
+		addDataSet();
+		
 	}
 
 	private void initLineChart() {
@@ -53,17 +71,56 @@ public class BSHistoryFragment extends BaseHistoryFragment {
 	}
 
 	private void addEmptyData() {
+		ArrayList<String> xVals = new ArrayList<>();
+		for (int i = 0, size = bsResults.size(); i< size; i++) {
+			Date date = DateUtil.longToDate(bsResults.get(i).date);
+			String datestr = DateUtil.getFormatDate("MM.dd", date);
+			xVals.add(datestr);
+		}
 
-		// create 30 x-vals
-		String[] xVals = new String[30];
-
-		for (int i = 0; i < 30; i++)
-			xVals[i] = "" + i;
+		for (int i = bsResults.size(); i < 8; i++) {
+			Calendar nowss = Calendar.getInstance();
+			String datestr = nowss.get(Calendar.MONTH) + 1 + "."
+					+ (nowss.get(Calendar.DAY_OF_MONTH) + i);
+			xVals.add(datestr);
+		}
+		
 
 		LineData data = new LineData(xVals);
 		mChart.setData(data);
 		mChart.invalidate();
 	}
+	
+	private void addDataSet() {
+
+		LineData data = mChart.getData();
+
+		if (data != null) {
+			// create 10 y-vals
+			ArrayList<Entry> yValsBs = new ArrayList<Entry>();
+			
+
+			//mChart.setScaleMinima(fhValues.size() / 15, 1);// …Ë÷√Àı∑≈±»¿˝
+
+			for (int i = 0; i < bsResults.size(); i++) {
+				yValsBs.add(new Entry(Float.parseFloat(bsResults.get(i).bg), i));
+			}
+
+			LineDataSet set = new LineDataSet(yValsBs,
+					getString(R.string.bs_text));
+			set.setLineWidth(2.5f);
+			set.setCircleSize(3f);
+			int color = getResources().getColor(R.color.red);
+			set.setColor(color);
+			set.setCircleColor(color);
+			set.setHighLightColor(color);
+
+			data.addDataSet(set);
+			mChart.notifyDataSetChanged();
+			mChart.animateY(1000);
+		}
+	}
+
 
 	@Override
 	public boolean handleConnect() {
