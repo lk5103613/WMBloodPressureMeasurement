b1cf25bd9d7be35175e77c8d63c9864b9a9dcf12
Like <524148211@qq.com>
2015-03-31 10:46:09 +0800

    Add welcome page.

---
+++ src/com/wm/activity/WelcomeActivity.java
@@ -0,0 +1,36 @@
+package com.wm.activity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class WelcomeActivity extends Activity {
+	
+	@InjectView(R.id.welcome_logo)
+	ImageView mLogo;
+	@InjectView(R.id.welcome_company_name)
+	ImageView mCompanyName;
+	
+	private Context mContext;
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_welcome);
+		ButterKnife.inject(this);
+		
+		mContext = this;
+		
+		Animation logoAnimation = AnimationUtils.loadAnimation(mContext, R.anim.welcome_logo_anim);
+		mLogo.startAnimation(logoAnimation);
+		Animation companyNameAnimation = AnimationUtils.loadAnimation(mContext, R.anim.welcome_company_name_anim);
+		mCompanyName.startAnimation(companyNameAnimation);
+	}
+	
+}
