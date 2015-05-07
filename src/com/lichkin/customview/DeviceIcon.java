26a90126e4dead74293415f0d4b6cec2aab4e2da
joselyncui <15895026586@163.com>
2015-05-07 09:36:21 +0800

    add custom view

---
+++ src/com/lichkin/customview/DeviceIcon.java
@@ -0,0 +1,40 @@
+package com.lichkin.customview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import com.lichkin.activity.R;
+import com.lichkin.entity.DeviceInfo;
+
+public class DeviceIcon extends ImageView {
+
+	public DeviceIcon(Context context) {
+		super(context);
+	}
+
+	public DeviceIcon(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	public DeviceIcon(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+	}
+
+	public void setType(String type) {
+		if (type.equals(DeviceInfo.TYPE_BS)) {
+			setImageResource(R.drawable.bs_icon);
+		} else if (type.equals(DeviceInfo.TYPE_BP)) {
+			setImageResource(R.drawable.bp_icon);
+		} else {
+			setImageResource(R.drawable.fh_icon);
+		}
+	}
+
+}
---
+++ src/com/lichkin/customview/LineMarkerView.java
@@ -0,0 +1,58 @@
+package com.lichkin.customview;
+
+import android.content.Context;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.utils.MarkerView;
+import com.github.mikephil.charting.utils.Utils;
+import com.lichkin.activity.R;
+
+/**
+ * 自定义MarkerView
+ * 
+ */
+public class LineMarkerView extends MarkerView {
+
+	private TextView tvContent;
+	private LineChart mChart;
+
+	public LineMarkerView(Context context, LineChart chart, int layoutResource) {
+		super(context, layoutResource);
+		tvContent = (TextView) findViewById(R.id.tvContent);
+		this.mChart = chart;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.github.mikephil.charting.utils.MarkerView#refreshContent(com.github
+	 * .mikephil.charting.data.Entry, int)
+	 * 
+	 * 每次重绘 markerview的时候会调用， 可以用户更新内容
+	 */
+	@Override
+	public void refreshContent(Entry e, int dataSetIndex) {
+
+		String value = Utils.formatNumber(e.getVal(), 0, true);
+
+		tvContent.setText(value);
+
+		int color = mChart.getData().getDataSetByIndex(dataSetIndex).getColor();
+		if (color == getResources().getColor(R.color.fragment_bg)) {
+			tvContent.setText("");
+		}
+	}
+
+	@Override
+	public float getXOffset() {
+		return -(getWidth() / 2);// 水平居中
+	}
+
+	@Override
+	public float getYOffset() {
+		return -getHeight(); // 让markerView居于被选择的数据之上
+	}
+}
